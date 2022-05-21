8752517c2107abd03f066f4ac7cf8ca21777ad09
TasnimAhmed <tasnim.ahamed.20@gmail.com>
2022-05-21 11:59:03 +0600

    Another Revision

--- js/script.js
+++ js/script.js
@@ -1,14 +1,6 @@
 $(document).ready(function() {
 
 
-    $(window).scroll(function(){
-        var navFixed = $(window).scrollTop();
-        if(navFixed > 50){
-            $(".main__menu").addClass("fadeInDown");
-        }else{
-            $(".main__menu").removeClass("fadeInDown");
-        }
-    });
 
     $(document).on('click','.navbar-collapse',function(e) {
         if( $(e.target).is('a') ) {
