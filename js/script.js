063423eab57eceaba0e2fe5e3d489cb4b5e3de01
TasnimAhmed <tasnim.ahamed.20@gmail.com>
2022-05-21 08:58:06 +0600

    Another Revision

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
 
     $(window).scroll(function(){
         var navFixed = $(window).scrollTop();
-        if(navFixed > 100){
-            $(".main__menu").addClass("fixed-top fadeInDown");
+        if(navFixed > 50){
+            $(".main__menu").addClass("fadeInDown");
         }else{
-            $(".main__menu").removeClass("fixed-top fadeInDown");
+            $(".main__menu").removeClass("fadeInDown");
         }
     });
 
